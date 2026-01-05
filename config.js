8d58eb7f508aa12c9e9527ca23ae60bed028c7ab
Ninh-CHan <ohzay125@gmail.com>
2026-01-05 13:05:27 +0700

    Full

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Xin chào bé Winter",
+    introTitle: 'Winter',
+    introDesc: `Con tằm đắm đuối vì tơ
+    Tôi xay vì rượu ngẩn nhơ vì tình
+    Có duyên ta mới có tình
+    Chào nhau một cái cho mình thêm duyên`,
+    btnIntro: '^^ Hệ hệ hệ ^^',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h, Anh qua đón đi uống rượu nha nha.',
+    btnAccept: 'Okiiiii thì ib anh nha <3. Mãi yêu bé Winter <3',
+    messLink: 'https://www.facebook.com/ninh.tran.103859/' 
+}
